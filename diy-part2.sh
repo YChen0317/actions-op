251df6281256a210c8bc306bd70a6f0d394ada84
yc <99346683+YChen0317@users.noreply.github.com>
2022-02-12 15:32:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+git clone https://github.com/rufengsuixing/luci-app-onliner
+git clone https://github.com/gdck/luci-app-control-weburl
+git clone https://github.com/aliass532/luci-app-eqos
+git clone https://github.com/msylgj/luci-app-tencentddns
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.6/g' package/base-files/files/bin/config_generate
 
