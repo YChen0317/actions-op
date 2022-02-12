fc999fcff3ed84329ecb7b3925bae1253f670ca2
yc <99346683+YChen0317@users.noreply.github.com>
2022-02-12 15:44:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,10 @@
 git clone https://github.com/rufengsuixing/luci-app-onliner
 git clone https://github.com/gdck/luci-app-control-weburl
 git clone https://github.com/aliass532/luci-app-eqos
+git clone https://github.com/jerrykuku/luci-app-argon-config
 git clone https://github.com/msylgj/luci-app-tencentddns
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.6/g' package/base-files/files/bin/config_generate
 
